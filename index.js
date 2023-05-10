ca2ab898edd3ce47ba121f2df3050737fe36fe56
codechini <arangathvenuchinmay@gmail.com>
2023-05-10 08:50:11 +0530

    moreCss

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function seachGuide() {
-  console.log("hi")
+  // console.log("hi")
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
@@ -11,14 +11,21 @@ function seachGuide() {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      console.log(filter.length);
+      // console.log(filter.length);
       li[i].style.display = "";
     }
     else {
       li[i].style.display = "none";
     }
+    // Checks if string less than 1 don't display the "ul"
+    // and make border-radius of the input to rounded corners
+    // else have border-radius of the input for right-top and left-top
     if (filter.length < 1) {
       document.getElementById("myGuides").style.display = "none";
+      document.getElementById("myInput").style.borderRadius = "25px";
+    }
+    else {
+      document.getElementById("myInput").style.borderRadius = "10px 10px 0px 0px";
     }
   }
   // document.getElementById("myGuides").style.display = "block";
